d11da83b1d8ce5d137827eddeb5e359e291cec8e
Aaron <aaroneg@users.noreply.github.com>
2021-06-10 16:41:19 -0500

    Update get-clients-with-version.ps1

--- get-clients-with-version.ps1
+++ get-clients-with-version.ps1
@@ -3,7 +3,7 @@ foreach ($client in $clientJson) {
   $id=$client.id
   $address=$client.inst.split(':')[1]
   $clienthostname=$client.client_metadata.hostname
-  if ($client.client_metadata.ceph_version) {$version=$client.client_metadata.ceph_version.split(' ')[2]}
+  if ($client.client_metadata.ceph_version) {$version=$client.client_metadata.ceph_version.split(' ')[4]}
   else { $version='BLANK' }
   "$clienthostname,$address,$id,$version" 
 }
