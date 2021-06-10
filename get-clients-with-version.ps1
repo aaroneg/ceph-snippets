9ee231228604423943111ab003409da4e8f6a577
Aaron <aaroneg@users.noreply.github.com>
2021-06-10 16:50:19 -0500

    Update get-clients-with-version.ps1

--- get-clients-with-version.ps1
+++ get-clients-with-version.ps1
@@ -1,15 +1,16 @@
 $clientJson=ceph tell mds.0 client ls | convertfrom-json
+$clients=@()
 foreach ($client in $clientJson) {
   $id=$client.id
   $address=$client.inst.split(':')[1]
   $clienthostname=$client.client_metadata.hostname
   if ($client.client_metadata.ceph_version) {$version=$client.client_metadata.ceph_version.split(' ')[4]}
   else { $version='BLANK' }
-  [PSCustomObject]@{
+  $clients+=[PSCustomObject]@{
     hostname=$clienthostname
     address=$address
     id=$id
     version=$version
    }
-  #"$clienthostname,$address,$id,$version" 
 }
+$clients|select -unique
