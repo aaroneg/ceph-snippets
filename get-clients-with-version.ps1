ea45eefdacdeabd8e488580c0c6673d5c2b148c0
Aaron <aaroneg@users.noreply.github.com>
2021-06-10 16:35:18 -0500

    Update get-clients-with-version.ps1

--- get-clients-with-version.ps1
+++ get-clients-with-version.ps1
@@ -1,1 +1,8 @@
-
+$clientJson=ceph tell mds.0 client ls | convertfrom-json
+foreach ($client in $clientJson) {
+  $id=$client.id
+  $address=$client.inst.split(':')[1]
+  $clienthostname=$client.client_metadata.hostname
+  $version=$client.client_metadata.ceph_version.split(' ')[2]
+  "$($client.id),$()" 
+}
